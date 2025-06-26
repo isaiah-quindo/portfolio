7cea29dcea03e1a775ee94e70f53ce62651c7e84
Isaiah Quindo <isaiah.quindo@gmail.com>
2025-06-26 13:17:18 +1000

    Update layout.tsx to enhance metadata and improve structure
    - Added detailed Open Graph metadata for better SEO and social sharing.
    - Refined layout structure for improved readability and consistency.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,41 +4,56 @@ import "./globals.css";
 import SmoothScroll from "./components/SmoothScroll";
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
+  title: "Isaiah Quindo | Product Designer & Frontend Developer",
+  description: "Product Designer & Frontend Developer",
+  openGraph: {
     title: "Isaiah Quindo | Product Designer & Frontend Developer",
-    description: "Product Designer & Frontend Developer",
+    description:
+      "I&apos;m a product designer and frontend developer driven by a passion for creating user-centric experience.",
+    images: [
+      {
+        url: "/images/fb-opengraph.png", // Replace with your image URL
+        width: 1200,
+        height: 630,
+        alt: "Isaiah Quindo",
+      },
+    ],
+    type: "website",
+    url: "https://isaiahquindo.com",
+  },
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <svg
-                    className="pointer-events-none absolute cursor-none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <filter id="noiseFilter">
-                        <feTurbulence type="turbulence" baseFrequency="0.5" />
-                        <feColorMatrix
-                            type="saturate"
-                            values="0"></feColorMatrix>
-                    </filter>
-                </svg>
-                <SmoothScroll>{children}</SmoothScroll>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <svg
+          className="pointer-events-none absolute cursor-none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <filter id="noiseFilter">
+            <feTurbulence type="turbulence" baseFrequency="0.5" />
+            <feColorMatrix type="saturate" values="0"></feColorMatrix>
+          </filter>
+        </svg>
+        <SmoothScroll>{children}</SmoothScroll>
+      </body>
+    </html>
+  );
 }
